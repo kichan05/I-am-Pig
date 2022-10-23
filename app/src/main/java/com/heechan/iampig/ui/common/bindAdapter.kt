d0e871c65e558a848138c30528ae3b3fc5af1054
Heechan <ckstmznf@naver.com>
2022-10-23 18:31:55 +0900

    Main food list

--- app/src/main/java/com/heechan/iampig/ui/common/bindAdapter.kt
+++ app/src/main/java/com/heechan/iampig/ui/common/bindAdapter.kt
@@ -1,9 +1,22 @@
 package com.heechan.iampig
 
+import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @BindingAdapter("bindVisibility")
 fun bindVisibility(view : View, visibility : Boolean) {
     view.visibility = if(visibility) View.VISIBLE else View.INVISIBLE
+}
+
+@BindingAdapter("bindDateTimeFormat")
+fun bindDateTimeFormat(textView: TextView, dateTime : String) {
+    val date = LocalDateTime.parse(dateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+
+    Log.d("bindDateTimeFormat", date.toString())
+
+    textView.text = "${date.year}년 ${date.monthValue}월 ${date.dayOfMonth}일 ${date.hour}시"
 }
